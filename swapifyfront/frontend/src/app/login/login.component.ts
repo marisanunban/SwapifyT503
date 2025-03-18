551a511ec555f75ce66434c1526a709ec59406ea
marisanunban <marisanuba20@gmail.com>
2025-03-18 12:12:04 +0100

    Add new functional front

---
+++ swapifyfront/frontend/src/app/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+loginWithFacebook() {
+throw new Error('Method not implemented.');
+}
+loginWithGoogle() {
+throw new Error('Method not implemented.');
+}
+forgotPassword() {
+throw new Error('Method not implemented.');
+}
+onSubmit() {
+throw new Error('Method not implemented.');
+}
+loginForm: any;
+email: any;
+
+}
