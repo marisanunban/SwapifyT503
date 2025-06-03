f11ec2790c62ad04812468edc0897876b8c3c535
fxKronik <victoraguilarpt@gmail.com>
2025-06-03 10:39:34 +0200

    Beta

--- backend/Products/products-service/src/main/java/com/example/demo/dtos/FavoriteRequestDto.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.dtos;
-
-import lombok.Data;
-
-@Data
-public class FavoriteRequestDto {
-    private String productId;
-}
--- backend/Transaction/transaction-service/src/main/java/com/example/demo/entities/Status.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.entities;
-
-public enum Status {
-    PENDING, COMPLETED, REJECTED
-}
